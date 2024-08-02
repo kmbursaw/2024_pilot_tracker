2e4c1e62e8b760f8a3a1dd4bab1898aa6d9992fa
Kevin Bursaw, EdD <132414922+kmbursaw@users.noreply.github.com>
2024-08-02 14:44:30 -0500

    Add files via upload

--- Final_Projectv1/js/mainv2.js
+++ Final_Projectv1/js/mainv2.js
@@ -5,8 +5,8 @@ window.onload = setMap();
 
 
 //chart frame dimensions
-var chartWidth = window.innerWidth * 0.425,
-    chartHeight = 473,
+var chartWidth = window.innerWidth,
+    chartHeight = 460,
     leftPadding = 39,
     rightPadding = 2,
     topBottomPadding = 5,
@@ -30,7 +30,7 @@ function setMap(){
         .attr("width", width)
         .attr("height", height);
 
-    //create Albers equal area conic projection with Alaska and Hawaii inset
+    //create Albers USA projection with Alaska and Hawaii inset
     var projection = d3.geoAlbersUsa()
         .scale(900)
         .translate([width / 2, height / 2]);
@@ -65,30 +65,45 @@ function setMap(){
             .attr("d", path)
             .attr("stroke", "#ccc")
             .attr("fill", "gray")
+            //.attr("cursor", "pointer")
             //.attr("background", "white")
-            //.on("click", clicked);
+            //.on("click", clicked)
             .enter()
             .attr("class", function(d){
                 return "usaStates" + d.properties.State
             })
             .attr("d", path);
 
-
+        createDropdown();
         
+        /*
+        var mapFrame = map.append("rect")
+            .attr("class", "chartframe")
+            .attr("width", chartInnerWidth)
+            .attr("height", chartInnerHeight)
+            .attr("transform", translate);
+        */
+
+        /*
+        var zoom = d3.zoom()
+            .scaleExtent([1, 8])
+            .on("zoom", zoomed);
+        
+        const g = svg.append("g");
+        */
     }
 
     var svgInfo = d3.select("body").append("svg")
         .attr("width", 100)
-        .attr("height", 900)
+        .attr("height", window.innerHeight)
         .style("position", "absolute")
         .style("left", "0px");
-        //.sytle("top", "0px");
 
     var infoRect = svgInfo.append("rect")
         .attr("x", 0)
-        .attr("y", 10)
+        .attr("y", 0)
         .attr("width", 100)
-        .attr("height", 500)
+        .attr("height", window.innerHeight)
         .style("fill", "steelblue");
 
     /*
@@ -120,7 +135,42 @@ function setMap(){
     
 };
 
+//function to create a dropdown menu for attribute selection
+function createDropdown(pop_Vote){
+    //add select element
+    var dropdown = d3.select("body")
+        .append("select")
+        .attr("class", "dropdown")
+        .attr("x", 0)
+        .style("width", "80px")
+        .style("height", "30px")
+        .on("change", function(){
+            changeAttribute(this.value, pop_Vote)
+        });
+    
+
+    //add initial option
+    var titleOption = dropdown.append("option")
+        .attr("class", "titleOption")
+        .attr("disabled", "true")
+        .text("Select");
+
+    //add attribute name options
+    var attrOptions = dropdown.selectAll("attrOptions")
+        .data(attrArray)
+        .enter()
+        .append("option")
+        .attr("value", function(d){ return d })
+        .text(function(d){ return d });
+    };
+
+
+
+
+
+/*
 function setChart(){
+    
     //create a second svg element to hold the bar chart
     var chart = d3.select(".chart-container")
         .append("svg")
@@ -135,6 +185,25 @@ function setChart(){
         .attr("height", chartInnerHeight)
         .attr("transform", translate);
 
-};
+    var chartTitle = chart.append("text")
+        .attr("x", 75)
+        .attr("y", 40)
+        .attr("class", "chartTitle")
+        .attr("fill", "white")
+        .text("Pilots");
 
-setChart();
+    
+    //create frame for chart border
+    var chartFrame = chart.append("rect")
+        .attr("class", "chartFrame")
+        .attr("width", chartInnerWidth)
+        .attr("height", chartInnerHeight)
+        //.attr("transform", translate)
+        .attr("x", 50)
+        .attr("y", 50);
+    
+    
+}; //last line
+*/
+
+//setChart();
